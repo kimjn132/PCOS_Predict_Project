6b84f35d1aefc569b0f4a9852aa6bf259a7027de
kimjn132 <kjhi8309@gmail.com>
2023-02-23 15:07:14 +0900

    map tab upload

--- pcos_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pcos_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_remote_config
+import geolocator_apple
 import package_info
 import sqflite
 
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
