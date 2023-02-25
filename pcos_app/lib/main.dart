afe7bfdddf13b1a8003e0ba9f0db7ed81d049d2b
kimjn132 <kjhi8309@gmail.com>
2023-02-25 15:29:12 +0900

    지도 방문한 병원 like 버튼 및 해당 병원 firebase저장

--- pcos_app/lib/main.dart
+++ pcos_app/lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:pcos_app/bottom_navigation.dart';
+import 'package:pcos_app/controller/map_provider.dart';
 import 'package:pcos_app/firebase_options.dart';
 import 'package:pcos_app/view/login/signin_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,17 +27,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          selectedItemColor: Color(0xFFF16A6E)
-        )
-        
+    return ChangeNotifierProvider(
+      create: (_) => FavoriteProvider(),
+      child: GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            selectedItemColor: Color(0xFFF16A6E)
+          )
+          
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SignInScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const SignInScreen(),
     );
   }
 }
