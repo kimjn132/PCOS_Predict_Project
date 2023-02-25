c7d9dbb74f17d7d7abd6e55f6911067bf587c4bb
kimjn132 <kjhi8309@gmail.com>
2023-02-26 00:09:34 +0900

    fix

--- pcos_app/lib/main.dart
+++ pcos_app/lib/main.dart
@@ -12,7 +12,7 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    //options: DefaultFirebaseOptions.currentPlatform,
   );
 
   await initializeDateFormatting();
